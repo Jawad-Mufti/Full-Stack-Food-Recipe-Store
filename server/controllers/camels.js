87cdad0090f4b89ddd1c945a48b75744a0c82acb
Mufti <gusmofmo@student.gu.se>
2021-01-29 00:25:17 +0100

    update server controller

---
+++ server/controllers/camels.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var Camel = require('../models/camel');
+
+// Return a list of all camels
+router.get('/', function(req, res, next) {
+    Camel.find(function(err, camels) {
+        if (err) { return next(err); }
+        res.json({"data": camels});
+    });
+});
+
+// Create a new camel
+router.post('/', function(req, res, next) {
+    var camel = new Camel(req.body);
+    camel.save(function(err) {
+        if (err) { return next(err); }
+        res.status(201).json(camel);
+    });
+});
+
+// Delete the camel with the given ID
+router.delete('/:id', function(req, res, next) {
+    var id = req.params.id;
+    Camel.findOneAndDelete({_id: id}, function(err, camel) {
+        if (err) { return next(err); }
+        if (camel == null) {
+            return res.status(404).json({"message": "Camel not found"});
+        }
+        res.json(camel);
+    });
+});
+
+module.exports = router
