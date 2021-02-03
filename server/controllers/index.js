abe5e94a7623b7ef7d8a5d1f76cf905ec850e4a2
Jawad Mufti <38205301+Jawad-Mufti@users.noreply.github.com>
2021-02-03 12:40:41 +0100

    Update index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -26,3 +26,6 @@ router.route('/admin').get(function (req, res) {
 router.route('/user').get(function (req, res) {
     res.sendfile(req.app.get('appPath') + '/user.html');
 });
+
+router.route('/*').get(function (req, res) {
+    var relativeAppPath = req.app.get('appPath');
