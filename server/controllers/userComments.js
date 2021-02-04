b362cc07b44292d789b03f44cc4bc13569393c91
Jawad Mufti <38205301+Jawad-Mufti@users.noreply.github.com>
2021-02-04 08:06:50 +0100

    make user able to post a comment on a recipe

--- server/controllers/userComments.js
+++ server/controllers/userComments.js
@@ -18,3 +18,33 @@ router.get("/", function (req, res, next) {
        ;
 });
 
+
+router.post("/",function(req,res, next){
+
+	var newComment = {
+		author  : req.body.author,
+		text    : req.body.text
+	}
+
+	Recipe.findById(req.params.id,function(err,recipe){
+		if(err){
+			return next(err);
+
+		} else if(recipe == null){
+			return res.status(404).json({"message":"Recipe is not Found"});
+		} 
+
+		else{
+			Comment.create(newComment,function(err,comment){
+				if(err){
+					return next(err);
+				}else{
+					recipe.comments.push(comment);
+					recipe.save();
+					res.status(201).json(recipe);
+				}
+			});
+		}
+	});
+});
+
