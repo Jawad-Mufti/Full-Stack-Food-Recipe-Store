56bdd449361e6ae7cbc9f4a564110cb97394ba4f
Mufti <gusmofmo@student.gu.se>
2021-02-04 07:00:27 +0100

    update user comment controller to comment all

--- server/controllers/userComments.js
+++ server/controllers/userComments.js
@@ -3,3 +3,14 @@ var    express = require('express'),
        Recipe  = require('../models/recipe'),
        Comment = require('../models/comment');
 
+// get all comments for a specific Recipe
+router.get("/", function (req, res, next) {
+    Recipe.findById(req.params.id).populate("comments").exec(function (err, recipe) {
+        if (err) {
+            return next(err);
+        } else if (recipe == null) {
+            return res.status(404).json({ "message": "Recipe is not Found" });
+        } else {
+            res.json(recipe);
+        }
+    });
